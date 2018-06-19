5119b72a197c61ee8cec2114fe297838aed503f0
Yan Wang <wangyan6783@gmail.com>
2018-06-19 17:41:45 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each { |season, holidays|
-    puts ""
+    puts "#{season.to_s.capitalize}"
   }
   
 

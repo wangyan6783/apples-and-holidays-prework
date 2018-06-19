836114a0dbeed3cb1f0aa051f1a5c5b1ac162af5
Yan Wang <wangyan6783@gmail.com>
2018-06-19 18:24:03 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -97,23 +97,6 @@ def all_holidays_with_bbq(holiday_hash)
   arr
 end
 
- # {
-  #   :winter => {
-  #     :christmas => ["Lights", "Wreath"],
-  #     :new_years => ["Party Hats"]
-  #   },
-  #   :summer => {
-  #     :fourth_of_july => ["Fireworks", "BBQ"]
-  #   },
-  #   :fall => {
-  #     :thanksgiving => ["Turkey"]
-  #   },
-  #   :spring => {
-  #     :memorial_day => ["BBQ"]
-  #   }
-  # }
-
-
 
 
 

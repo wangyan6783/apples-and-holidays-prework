7ecd2b33d2da1b450eb620ea5cac64b76bc1be18
Yan Wang <wangyan6783@gmail.com>
2018-06-19 17:27:51 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -31,7 +31,6 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
 
 end
 
-
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array

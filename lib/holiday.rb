f69562388ce7745a95659720b3bfbdac29b12ad6
Yan Wang <wangyan6783@gmail.com>
2018-06-19 17:24:30 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -21,10 +21,20 @@ def second_supply_for_fourth_of_july(holiday_hash)
   holiday_hash[:summer][:fourth_of_july][1]
 end
 
+ # Write a method that adds a supply to all Winter holidays
+  describe "#add_supply_to_winter_holidays" do
+    it "iterates through winter holidays adds a supply to each one" do
+      add_supply_to_winter_holidays(holiday_supplies, "Balloons")
+      expect(holiday_supplies[:winter][:christmas]).to include("Balloons")
+      expect(holiday_supplies[:winter][:new_years]).to include("Balloons")
+    end
+  end
+  
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
+  holiday_hash[:winter]
 
 end
 
